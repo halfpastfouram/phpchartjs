b412a07f569d3f0ad92f4fd66251793b4d4ba815
Bob <bobkruithof@gmail.com>
2016-11-23 18:39:51 +0100

    - Fixes #5: Introduced Hover options - Fixes #9: Introduced Animation options

--- src/Options/Animation.php
+++ src/Options/Animation.php
@@ -4,6 +4,7 @@ namespace Halfpastfour\PHPChartJS\Options;
 
 use Halfpastfour\PHPChartJS\ArraySerializableInterface;
 use Halfpastfour\PHPChartJS\Delegate\ArraySerializable;
+use Zend\Json\Expr;
 use Zend\Json\Json;
 
 /**
@@ -15,10 +16,110 @@ class Animation implements ArraySerializableInterface, \JsonSerializable
 	use ArraySerializable;
 
 	/**
+	 * @var int
+	 */
+	private $duration;
+
+	/**
+	 * @var string
+	 */
+	private $easing;
+
+	/**
+	 * @var Expr
+	 */
+	private $onProgress;
+
+	/**
+	 * @var Expr
+	 */
+	private $onComplete;
+
+	/**
+	 * @return int
+	 */
+	public function getDuration()
+	{
+		return $this->duration;
+	}
+
+	/**
+	 * @param int $duration
+	 *
+	 * @return $this
+	 */
+	public function setDuration( $duration )
+	{
+		$this->duration = $duration;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEasing()
+	{
+		return $this->easing;
+	}
+
+	/**
+	 * @param string $easing
+	 *
+	 * @return $this
+	 */
+	public function setEasing( $easing )
+	{
+		$this->easing = $easing;
+
+		return $this;
+	}
+
+	/**
+	 * @return Expr
+	 */
+	public function getOnProgress()
+	{
+		return $this->onProgress;
+	}
+
+	/**
+	 * @param Expr $onProgress
+	 *
+	 * @return $this
+	 */
+	public function setOnProgress( $onProgress )
+	{
+		$this->onProgress = $onProgress;
+
+		return $this;
+	}
+
+	/**
+	 * @return Expr
+	 */
+	public function getOnComplete()
+	{
+		return $this->onComplete;
+	}
+
+	/**
+	 * @param Expr $onComplete
+	 *
+	 * @return $this
+	 */
+	public function setOnComplete( $onComplete )
+	{
+		$this->onComplete = $onComplete;
+
+		return $this;
+	}
+
+	/**
 	 * @return string
 	 */
 	public function jsonSerialize()
 	{
-		return Json::encode( $this->getArrayCopy() );
+		return Json::encode( $this->getArrayCopy(), false, [ 'enableJsonExprFinder' => true ] );
 	}
 }
--- src/Options/Hover.php
+++ src/Options/Hover.php
@@ -4,6 +4,7 @@ namespace Halfpastfour\PHPChartJS\Options;
 
 use Halfpastfour\PHPChartJS\ArraySerializableInterface;
 use Halfpastfour\PHPChartJS\Delegate\ArraySerializable;
+use Zend\Json\Expr;
 use Zend\Json\Json;
 
 /**
@@ -13,6 +14,106 @@ use Zend\Json\Json;
 class Hover implements ArraySerializableInterface, \JsonSerializable
 {
 	use ArraySerializable;
+
+	/**
+	 * @var string
+	 */
+	private $mode;
+
+	/**
+	 * @var bool
+	 */
+	private $intersect;
+
+	/**
+	 * @var int
+	 */
+	private $animationDuration;
+
+	/**
+	 * @var Expr
+	 */
+	private $onHover;
+
+	/**
+	 * @return string
+	 */
+	public function getMode()
+	{
+		return $this->mode;
+	}
+
+	/**
+	 * @param string $mode
+	 *
+	 * @return $this
+	 */
+	public function setMode( $mode )
+	{
+		$this->mode = strval( $mode );
+
+		return $this;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isIntersect()
+	{
+		return $this->intersect;
+	}
+
+	/**
+	 * @param boolean $intersect
+	 *
+	 * @return $this
+	 */
+	public function setIntersect( $intersect )
+	{
+		$this->intersect = !!$intersect;
+
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getAnimationDuration()
+	{
+		return $this->animationDuration;
+	}
+
+	/**
+	 * @param int $animationDuration
+	 *
+	 * @return $this
+	 */
+	public function setAnimationDuration( $animationDuration )
+	{
+		$this->animationDuration = intval( $animationDuration );
+
+		return $this;
+	}
+
+	/**
+	 * @return \Zend\Json\Expr
+	 */
+	public function getOnHover()
+	{
+		return $this->onHover;
+	}
+
+	/**
+	 * @param Expr $onHover
+	 *
+	 * @return $this
+	 */
+	public function setOnHover( $onHover )
+	{
+		$this->onHover = new Expr( strval( $onHover ) );
+
+		return $this;
+	}
 
 	/**
 	 * @return string
