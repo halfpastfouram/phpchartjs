13ba0d5f24e90fac17abe204a0edc0aeab0d0cb1
Bob <bobkruithof@gmail.com>
2016-11-24 21:38:40 +0100

    Created more tests

--- test/unit/DataSetTest.php
+++ test/unit/DataSetTest.php
@@ -2,9 +2,15 @@
 
 namespace Test;
 
+use Halfpastfour\PHPChartJS\ArraySerializableInterface;
 use Halfpastfour\PHPChartJS\Chart\Bar;
 use Halfpastfour\PHPChartJS\ChartInterface;
+use Halfpastfour\PHPChartJS\ChartOwnedInterface;
+use Halfpastfour\PHPChartJS\Collection;
+use Halfpastfour\PHPChartJS\Collection\Data;
+use Halfpastfour\PHPChartJS\CollectionInterface;
 use Halfpastfour\PHPChartJS\DataSet;
+use Zend\Json\Json;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -17,32 +23,119 @@ class DataSetTest extends \PHPUnit_Framework_TestCase
 	/**
 	 *
 	 */
-	public function testDataSet()
+	public function testImplementation()
 	{
-		// Setup a new dataSet
 		$dataSet = new DataSet();
-		$this->assertNull( null, $dataSet->getOwner(), '' );
+		$this->assertInstanceOf( ChartOwnedInterface::class, $dataSet, 'Class implements ChartOwnedInterface' );
+		$this->assertInstanceOf(
+			ArraySerializableInterface::class,
+			$dataSet,
+			'Class implements ArraySerializableInterface'
+		);
+		$this->assertInstanceOf( \JsonSerializable::class, $dataSet, 'Class implements JsonSerializable' );
+	}
 
-		// Assign dataSet to Bar chart
-		$chart   = new Bar();
+	/**
+	 *
+	 */
+	public function testOwner()
+	{
+		$dataSet = new DataSet();
+
+		$this->assertNull( $dataSet->getOwner(), 'The dataset has no owner' );
+
+		$chart = new Bar();
 		$chart->addDataSet( $dataSet );
-		$this->assertEquals( $chart, $dataSet->getOwner(), 'The owner of the dataSet is set and returned correctly' );
-		$this->assertInstanceOf( ChartInterface::class, $dataSet->getOwner(),
-			'The owner of the dataSet implements the correct interface' );
 
-		// Type
+		$this->assertEquals( $chart, $dataSet->getOwner(), 'The owner of the dataSet is set and returned correctly' );
+		$this->assertInstanceOf(
+			ChartInterface::class, $dataSet->getOwner(),
+			'The owner of the dataSet implements the correct interface'
+		);
+	}
+
+	/**
+	 *
+	 */
+	public function testType()
+	{
+		$dataSet = new DataSet();
+
 		$this->assertNull( $dataSet->getType(), 'The type is not set' );
 
 		$dataSet->setType( Bar::TYPE );
 		$this->assertEquals( Bar::TYPE, $dataSet->getType(), 'The type is set and returned correctly' );
+	}
 
-		// Label
+	/**
+	 *
+	 */
+	public function testData()
+	{
+		$dataSet = new DataSet();
+
+		$dataCollection	= $dataSet->data();
+		$this->assertInstanceOf( Data::class, $dataCollection, 'The data collection is the right class' );
+		$this->assertInstanceOf( Collection::class, $dataCollection, 'The data collection extends Collection' );
+		$this->assertInstanceOf(
+			CollectionInterface::class, $dataCollection, 'The data collection implements CollectionInterface'
+		);
+		$this->assertInstanceOf(
+			\JsonSerializable::class, $dataCollection, 'The data collection implements JsonSerializable'
+		);
+
+
+		$this->assertEmpty( $dataSet->data()->getArrayCopy(), 'The data inside the dataSet is empty' );
+
+		$dataCollection->append( 1 );
+		$this->assertEquals( [ 1 ] , $dataCollection->getArrayCopy(), 'The data consists of one value: 1' );
+
+		$dataCollection->prepend( 0 );
+		$this->assertEquals( [ 0, 1 ] , $dataCollection->getArrayCopy(), 'The data consists of values: 0, 1' );
+
+		$dataCollection->offsetSet( 2, 2 );
+		$this->assertEquals( [ 0, 1, 2 ] , $dataCollection->getArrayCopy(), 'The data consists of values: 0, 1, 2' );
+		$this->assertEquals( 1, $dataCollection->offsetGet( 1 ), 'The value at position 1 equals 1' );
+
+		$dataCollection->offsetUnset( 1 );
+		$this->assertArrayNotHasKey( 1, $dataCollection->getArrayCopy(), 'Value at offset 1 has been removed' );
+
+		$expectedValue = [ 0 => 0, 2 => 2 ];
+		$this->assertEquals( $expectedValue, $dataCollection->getArrayCopy(), 'The data consists of values: 0, 2' );
+
+		$this->assertEquals(
+			Json::encode( $expectedValue ), $dataCollection->jsonSerialize(), 'The serialized data is correct'
+		);
+
+		$newValues = [ 1, 2, 3, 'Foo', 'Bar', 'Baz' ];
+		$result    = $dataCollection->exchangeArray( $newValues );
+		$this->assertEquals( $expectedValue, $result, 'Exchanging array returns correct value' );
+		$this->assertEquals( $newValues, $dataCollection->getArrayCopy(), 'New values have been set correctly' );
+		$this->assertEquals(
+			Json::encode( $newValues ), $dataCollection->jsonSerialize(), 'The serialized data is correct'
+		);
+	}
+
+	/**
+	 *
+	 */
+	public function testLabel()
+	{
+		$dataSet = new DataSet();
+
 		$this->assertNull( $dataSet->getLabel(), 'The label is not set' );
 
 		$dataSet->setLabel( 'Foo' );
 		$this->assertEquals( 'Foo', $dataSet->getLabel(), 'The label is set and returned correctly' );
+	}
 
-		// Background colors
+	/**
+	 *
+	 */
+	public function testBackgroundColor()
+	{
+		$dataSet = new DataSet();
+
 		$this->assertNull( $dataSet->getBackgroundColor(), 'The background color is not set' );
 
 		$dataSet->setBackgroundColor( '#fff' );
@@ -54,8 +147,15 @@ class DataSetTest extends \PHPUnit_Framework_TestCase
 			$backgroundColorArray, $dataSet->getBackgroundColor(),
 			'The background color is set again and returned correctly'
 		);
+	}
 
-		// Border colors
+	/**
+	 *
+	 */
+	public function testBorderColor()
+	{
+		$dataSet = new DataSet();
+
 		$this->assertNull( $dataSet->getBorderColor(), 'The border color is not set' );
 
 		$dataSet->setBorderColor( '#fff' );
@@ -66,11 +166,47 @@ class DataSetTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals(
 			$borderColorArray, $dataSet->getBorderColor(), 'The border color is set again and returned correctly'
 		);
+	}
 
-		// Border skipped
+	/**
+	 *
+	 */
+	public function testBorderSkipped()
+	{
+		$dataSet = new DataSet();
+
 		$this->assertNull( $dataSet->getBorderSkipped(), 'The border skipped value is not set' );
 
-		$dataSet->setBorderSkipped( 'bottom' );
-		$this->assertEquals( 'bottom', $dataSet->getBorderSkipped() );
+		$this->assertInstanceOf(
+			DataSet::class, $dataSet->setBorderSkipped( 'bottom' ), 'The correct class is returned'
+		);
+		$this->assertEquals( 'bottom', $dataSet->getBorderSkipped(), 'The correct value is returned' );
+	}
+
+	/**
+	 *
+	 */
+	public function testAxes()
+	{
+		$dataSet = new DataSet();
+
+		$this->assertNull( $dataSet->getXAxisID(), 'The xAxisID value is not set' );
+
+		$this->assertInstanceOf( DataSet::class, $dataSet->setXAxisID( 'myXAxis' ) );
+		$this->assertEquals( 'myXAxis', $dataSet->getXAxisID(), 'The correct value is returned' );
+		$this->assertEquals(
+			Json::encode( [ 'xAxisID' => 'myXAxis' ] ), $dataSet->jsonSerialize(), 'The serialized data is correct'
+		);
+
+		$this->assertNull( $dataSet->getYAxisID(), 'The yAxisID value is not set' );
+
+		$this->assertInstanceOf( DataSet::class, $dataSet->setYAxisID( 'myYAxis' ) );
+		$this->assertEquals( 'myYAxis', $dataSet->getYAxisID(), 'The correct value is returned' );
+		$this->assertEquals(
+			Json::encode( [
+				'xAxisID' => 'myXAxis',
+				'yAxisID' => 'myYAxis'
+			] ), $dataSet->jsonSerialize(), 'The serialized data is correct'
+		);
 	}
 }
