0980fa6e209108c60db764d4e267bea9f9b5765c
Bob <bobkruithof@gmail.com>
2016-12-14 13:42:57 +0100

    Fixes #22

---
+++ test/unit/Options/Layout/PaddingTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Options\Layout;
+
+use Halfpastfour\PHPChartJS\Options\Layout\Padding;
+
+/**
+ * Class PaddingTest
+ * @package Options\Layout
+ */
+class PaddingTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var Padding
+	 */
+	protected $padding;
+
+	/**
+	 *
+	 */
+	public function setUp()
+	{
+		$this->padding = new Padding();
+	}
+
+	/**
+	 *
+	 */
+	public function testBottom()
+	{
+		$this->assertNull( $this->padding->getBottom() );
+		$this->padding->setBottom( 20 );
+		$this->assertSame( 20, $this->padding->getBottom() );
+	}
+
+	/**
+	 *
+	 */
+	public function testLeft()
+	{
+		$this->assertNull( $this->padding->getLeft() );
+		$this->padding->setLeft( 20 );
+		$this->assertSame( 20, $this->padding->getLeft() );
+	}
+
+	/**
+	 *
+	 */
+	public function testRight()
+	{
+		$this->assertNull( $this->padding->getRight() );
+		$this->padding->setRight( 20 );
+		$this->assertSame( 20, $this->padding->getRight() );
+	}
+
+	/**
+	 *
+	 */
+	public function testTop()
+	{
+		$this->assertNull( $this->padding->getTop() );
+		$this->padding->setTop( 20 );
+		$this->assertSame( 20, $this->padding->getTop() );
+	}
+}
