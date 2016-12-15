4da64c0abb53ff7c76afa68e29317bbb4dfe0e31
Guus Bonnema <gbonnema@xs4all.nl>
2016-12-15 13:08:16 +0100

    Added phpdoc and reformatted

--- test/unit/Options/LegendTest.php
+++ test/unit/Options/LegendTest.php
@@ -5,53 +5,79 @@ namespace Options;
 use Halfpastfour\PHPChartJS\Options\Legend;
 use Test\TestUtils;
 
+/**
+ * Class LegendTest
+ * @package Options
+ */
 class LegendTest extends \PHPUnit_Framework_TestCase
 {
+	/**
+	 * @var
+	 */
 	private $legend;
 
+	/**
+	 * @var array
+	 */
 	private $data_types = [
-		'display'		=> false,
-		'position'		=> '',
-		'fullWidth'		=> false,
-		'onClick'		=> '',
-		'onHover'		=> '',
-		'reverse'		=> false,
+		'display'   => false,
+		'position'  => '',
+		'fullWidth' => false,
+		'onClick'   => '',
+		'onHover'   => '',
+		'reverse'   => false,
 	];
 
+	/**
+	 * @var array
+	 */
 	private $input_data = [
-		'display'		=> true,
-		'position'		=> 'position',
-		'fullWidth'		=> true,
-		'onClick'		=> 'onClick',
-		'onHover'		=> 'onData',
-		'reverse'		=> true,
+		'display'   => true,
+		'position'  => 'position',
+		'fullWidth' => true,
+		'onClick'   => 'onClick',
+		'onHover'   => 'onData',
+		'reverse'   => true,
 	];
 
+	/**
+	 * @var array
+	 */
 	private $empty_data = [
-		'display'		=> null,
-		'position'		=> null,
-		'fullWidth'		=> null,
-		'onClick'		=> null,
-		'onHover'		=> null,
-		'reverse'		=> null,
+		'display'   => null,
+		'position'  => null,
+		'fullWidth' => null,
+		'onClick'   => null,
+		'onHover'   => null,
+		'reverse'   => null,
 	];
 
+	/**
+	 *
+	 */
 	public function setUp()
 	{
 		$this->legend = new Legend();
 	}
 
+	/**
+	 *
+	 */
 	public function testEmpty()
 	{
 		$expected = $this->empty_data;
-		$result = TestUtils::getAttributes($this->legend, $this->data_types);
-		self::assertSame($expected, $result);
+		$result   = TestUtils::getAttributes( $this->legend, $this->data_types );
+		self::assertSame( $expected, $result );
 	}
 
-	public function testGetAndSet() {
+	/**
+	 *
+	 */
+	public function testGetAndSet()
+	{
 		$expected = $this->input_data;
-		TestUtils::setAttributes($this->legend, $this->input_data);
-		$result = TestUtils::getAttributes($this->legend, $this->data_types);
-		self::assertSame($expected, $result);
+		TestUtils::setAttributes( $this->legend, $this->input_data );
+		$result = TestUtils::getAttributes( $this->legend, $this->data_types );
+		self::assertSame( $expected, $result );
 	}
 }
