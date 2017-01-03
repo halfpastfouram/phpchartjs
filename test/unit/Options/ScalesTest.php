16bf377252ce37f11e599c034fc5fe497a6f3e56
Guus Bonnema <gbonnema@xs4all.nl>
2017-01-03 10:31:32 +0100

    Adds testcase to 39.

--- test/unit/Options/ScalesTest.php
+++ test/unit/Options/ScalesTest.php
@@ -28,10 +28,46 @@ class ScalesTest extends \PHPUnit_Framework_TestCase
 	/**
 	 *
 	 */
-	public function testEmpty()
+	public function testEmptyXAxis()
 	{
 		$expected = new Scales\XAxisCollection();
 		$result   = $this->scales->getXAxes();
+		self::assertInstanceOf( Scales\XAxisCollection::class, $result );
 		self::assertEquals( $expected, $result );
+	}
+
+	/**
+	 *
+	 */
+	public function testEmptyYAxis()
+	{
+		$expected = new Scales\YAxisCollection();
+		$result   = $this->scales->getYAxes();
+		self::assertInstanceOf( Scales\YAxisCollection::class, $result );
+		self::assertEquals( $expected, $result );
+	}
+
+	/**
+	 *
+	 */
+	public function testJsonSerialize()
+	{
+		$xc = new Scales\XAxisCollection();
+		$x1 = new Scales\XAxis();
+		$x1->setBarThickness( 2 );
+		$xc[]              = $x1;
+		$expected['xAxes'] = json_decode( $xc->jsonSerialize(), true );
+
+		$yc = new Scales\YAxisCollection();
+		$y1 = new Scales\YAxis();
+		$y1->setBarThickness( 3 );
+		$yc[] = $y1;
+
+		$expected['xAxes']          = json_decode( $xc->jsonSerialize(), true );
+		$expected['yAxes']          = json_decode( $yc->jsonSerialize(), true );
+		$this->scales->getXAxes()[] = $x1;
+		$this->scales->getYAxes()[] = $y1;
+		$result                     = json_decode( $this->scales->jsonSerialize(), true );
+		self::assertSame( $expected, $result );
 	}
 }
