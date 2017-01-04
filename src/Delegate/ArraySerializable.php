f18cca2af49a023cf9d4a0db917366e58529a9f9
Bob <bobkruithof@gmail.com>
2017-01-04 13:55:42 +0100

    Fixes #45 and replaces get_class_vars with get_object_vars. Also reformatted.

--- src/Delegate/ArraySerializable.php
+++ src/Delegate/ArraySerializable.php
@@ -17,33 +17,32 @@ trait ArraySerializable
 	 */
 	public function getArrayCopy()
 	{
-		$data = [];
-
-		$properties			= get_class_vars( self::class );
-		$reflectionClass	= new \Zend_Reflection_Class( $this );
+		$data            = [];
+		$properties      = get_object_vars( $this );
+		$reflectionClass = new \Zend_Reflection_Class( $this );
 		foreach( $properties as $property => $value ) {
 			// Skip property if it is not accessible
 			if( !$reflectionClass->hasProperty( $property ) ) continue;
 
 			// Only process properties that aren't null
-			if( !is_null( $this->$property ) ) {
+			if( !is_null( $value ) ) {
 				// Gather phpdoc from property
-				$phpDoc	= $reflectionClass->getProperty( $property )->getDocComment();
-				$type	= $phpDoc->getTag( 'var' )->getDescription();
-				$object	= false;
+				$phpDoc = $reflectionClass->getProperty( $property )->getDocComment();
+				$type   = $phpDoc->getTag( 'var' )->getDescription();
+				$object = false;
 
 				// Prepend 'get' to the getter method.
 				$getter = 'get' . ucfirst( $property );
-				if( is_object( $this->$property ) && !$this->$property instanceof Expr ) $object = true;
+				if( is_object( $value ) && !$this->$property instanceof Expr ) $object = true;
 
 				// Determine whether the getter method is equal to the property name or is prepended by 'is' or 'get'
 				if( strcmp( $type, 'bool' ) === 0 || strcmp( $type, 'boolean' ) === 0 ) {
 					// Prepend 'is' to the getter method
-					$getter	= 'is' . ucfirst( $property );
-				} else if ( method_exists( $this, $property ) && is_object( $this->$property ) ) {
+					$getter = 'is' . ucfirst( $property );
+				} else if( method_exists( $this, $property ) && is_object( $value ) ) {
 					// The getter method is equal to the property name and the value is an actual object
-					$getter	= $property;
-					$object	= true;
+					$getter = $property;
+					$object = true;
 				}
 
 				// Abort if the method does not exist
