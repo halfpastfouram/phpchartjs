dc5ea9c9aee8e56e25c265f8f511215e85718803
Guus Bonnema <gbonnema@xs4all.nl>
2017-01-05 14:39:24 +0100

    Fixes #51.

---
+++ test/unit/Collection/DataTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Collection;
+
+use Halfpastfour\PHPChartJS\Collection\Data;
+
+/**
+ * Class DataTest
+ * @package Collection
+ */
+class DataTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var  Data */
+	private $data;
+
+	/**
+	 *
+	 */
+	public function setUp()
+	{
+		$this->data = new Data();
+	}
+
+	/**
+	 *
+	 */
+	public function testJsonSerializeEmpty()
+	{
+		$expected = "[]";
+		$result   = $this->data->jsonSerialize();
+		self::assertSame( $expected, $result );
+	}
+}
