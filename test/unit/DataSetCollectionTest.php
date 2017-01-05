4d70710542c628a80d04c496507a4f65177cb1af
Guus Bonnema <gbonnema@xs4all.nl>
2017-01-05 13:51:19 +0100

    Fixes #48.

---
+++ test/unit/DataSetCollectionTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gbonnema
+ * Date: 5-1-2017
+ * Time: 13:38
+ */
+
+namespace Halfpastfour\PHPChartJS;
+
+class DataSetCollectionTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var  DataSetCollection */
+	private $dataSetCollection;
+
+	public function setUp()
+	{
+		$this->dataSetCollection = new DataSetCollection();
+	}
+
+	public function testGetArrayCopyEmpty()
+	{
+		$expected = [];
+		$result = $this->dataSetCollection->getArrayCopy();
+		self::assertSame($expected, $result);
+	}
+
+	public function testGetArrayCopyNonEmpty() {
+		$expected = [[]];
+		$this->dataSetCollection->append(new DataSet());
+		$result = $this->dataSetCollection->getArrayCopy();
+		self::assertSame($expected, $result);
+	}
+
+	public function testJsonSerializeEmpty() {
+		$expected = '[]';
+		$result = $this->dataSetCollection->jsonSerialize();
+		self::assertSame($expected, $result);
+	}
+
+	public function testJsonSerializeNonEmpty() {
+		$expected = '[[]]';
+		$this->dataSetCollection->append(new DataSet());
+		$result = $this->dataSetCollection->jsonSerialize();
+		self::assertSame($expected, $result);
+	}
+}
